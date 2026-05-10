97accc75d5d88725f82e55c44f34fcf853342351
UBSITVALENCIACYRUSKYLE <20241752@s.ubaguio.edu>
2026-05-10 19:43:39 +0800

    feat: add rank up toast notification when user reaches a new rank

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar';
+import { ProgressService } from './services/progress';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
-  title = "Scholar's Ascent";
+  private progress = inject(ProgressService);
+
+  showRankUp = false;
+  newRank = '';
+  newRankName = '';
+  private previousRank = '';
+
+  constructor() {
+    effect(() => {
+      const rank = this.progress.currentRank();
+      if (this.previousRank && rank !== this.previousRank) {
+        this.newRank = rank;
+        this.newRankName = this.progress.rankName();
+        this.showRankUp = true;
+        setTimeout(() => this.showRankUp = false, 3500);
+      }
+      this.previousRank = rank;
+    });
+  }
 }
